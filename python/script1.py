a27d084ba965e7787d5e50a6c5233928db40862c
pietererasmus <p.erasmusjnr@gmail.com>
2021-03-09 13:17:59 +0200

    Remove model and fixed model numbering and fastai v2 changes

--- python/script1.py
+++ python/script1.py
@@ -3,7 +3,6 @@ import sys, os
 import json
 import cv2
 from fastai.vision.all import *
-from PIL import Image as PImage
 
 #This code is for the fastai V2 library
 class saveEachEpoch(Callback):
@@ -11,8 +10,8 @@ class saveEachEpoch(Callback):
         return true
         
 dir_path = os.path.dirname(os.path.realpath(__file__));
-learn = load_learner('./models/export_256_9496');
-classes = learn.data.classes
+learn = load_learner('./models/export_256_9143');
+classes = learn.data.vocab
 
 def apply_FFT_transform(input_image_path):
     img = cv2.imread(input_image_path)
@@ -25,9 +24,9 @@ def apply_FFT_transform(input_image_path):
 
 def img2d_3d(input_image):
     output_image = np.zeros((input_image.shape[0],input_image.shape[1], 3), dtype=np.uint8)
-    output_image[:,:,0] = input_image;
-    output_image[:,:,1] = input_image;
-    output_image[:,:,2] = input_image;
+    output_image[:,:,0] = input_image
+    output_image[:,:,1] = input_image
+    output_image[:,:,2] = input_image
     return output_image
 
 im_path = dir_path + '/' + sys.argv[1]
@@ -37,8 +36,5 @@ img_fft = apply_FFT_transform(im_path)
 
 img_3d = img2d_3d(img_fft)
 
-pil_im = PImage.fromarray(img_3d)
-
-x = pil2tensor(pil_im, np.float32)
-pred = learn.predict(Image(x/255))
+pred = learn.predict(PILImage.create(img_3d))
 print({c: round(float(pred[2][i]), 5) for (i, c) in enumerate(classes)})
